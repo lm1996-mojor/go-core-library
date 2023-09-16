0aecbb0d821ab40effc955e54cbee14ef65985c8
lm <1138484503@qq.com>
2023-09-16 16:32:15 +0800

    修改定时任务组件部分代码逻辑

--- tasker_factory/tasker.go
+++ tasker_factory/tasker.go
@@ -8,7 +8,7 @@ import (
 type Tasker struct {
 	t            *time.Ticker  // 任务主要执行对象
 	stopChan     chan struct{} // 停止通道
-	TaskerStatus string        // 任务状态（ running:执行  stop: 停止）
+	TaskerStatus string        // 任务状态（ ready:就绪/running:执行/stop:停止/block:阻塞）
 	TaskerId     string        // 任务id
 	TaskerName   string        // 任务名称
 	OwnerObjName string        // 任务所属对象名称
@@ -18,7 +18,7 @@ func NewTasker(d time.Duration, taskerId string, taskerName string, ownerObjName
 	return &Tasker{
 		t:            time.NewTicker(d),
 		stopChan:     make(chan struct{}),
-		TaskerStatus: "running",
+		TaskerStatus: "ready",
 		TaskerId:     taskerId,
 		TaskerName:   taskerName,
 		OwnerObjName: ownerObjName,
