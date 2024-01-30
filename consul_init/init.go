eafe321dcfee5c51ec36f835b73496360933b54d
lm <1138484503@qq.com>
2024-01-30 14:32:50 +0800

    更新：consul服务注册代码注册时机逻辑

--- consul_init/init.go
+++ consul_init/init.go
@@ -12,6 +12,28 @@ import (
 )
 
 func Init(app *iris.Application) {
+	host := ""
+	ipAddrList := sys_environment.GetInternalIP()
+	if libConfig.Sysconfig.SystemEnv.Env != "prod" {
+		host = ipAddrList[0]
+	} else {
+		host = sys_environment.GetExternal()
+	}
+	searchConditionValue := "ID contains " + libConfig.Sysconfig.App.Name + "-" + host + ":" + libConfig.Sysconfig.App.Port
+
+	consulServiceInfoList, err := FindServiceList(searchConditionValue)
+	if err != nil {
+		panic("查询服务列表失败：" + err.Error())
+	}
+	if len(consulServiceInfoList) > 0 {
+		for serviceId, _ := range consulServiceInfoList {
+			if err1 := ServiceDeregister(serviceId); err1 != nil {
+				panic("服务注销失败：" + err1.Error())
+			}
+		}
+	}
+	consulServiceId := Register()
+	store.Set(_const.ConsulEndId, consulServiceId)
 	mvc.New(app.Party("/consul_init")).Handle(NewController())
 	log.Info("初始化服务治理-服务健康检查接口")
 }
@@ -20,28 +42,6 @@ const runLevel = 10
 
 func init() {
 	if libConfig.Sysconfig.Consul.Addr != "" && libConfig.Sysconfig.Consul.Addr != "null" && len(libConfig.Sysconfig.Consul.Addr) > 0 {
-		host := ""
-		ipAddrList := sys_environment.GetInternalIP()
-		if libConfig.Sysconfig.SystemEnv.Env != "prod" {
-			host = ipAddrList[0]
-		} else {
-			host = sys_environment.GetExternal()
-		}
-		searchConditionValue := "ID contains " + libConfig.Sysconfig.App.Name + "-" + host + ":" + libConfig.Sysconfig.App.Port
-
-		consulServiceInfoList, err := FindServiceList(searchConditionValue)
-		if err != nil {
-			panic("查询服务列表失败：" + err.Error())
-		}
-		if len(consulServiceInfoList) > 0 {
-			for serviceId, _ := range consulServiceInfoList {
-				if err1 := ServiceDeregister(serviceId); err1 != nil {
-					panic("服务注销失败：" + err1.Error())
-				}
-			}
-		}
-		consulServiceId := Register()
-		store.Set(_const.ConsulEndId, consulServiceId)
 		global.RegisterInit(global.Initiator{Action: Init, Level: runLevel})
 	}
 	log.Info("无服务治理要求...")
