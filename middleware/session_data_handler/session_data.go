a90a63f6fae77ab8fd97b460e2290723860fcf1f
lm <1138484503@qq.com>
2024-02-28 15:58:10 +0800

    更新：修改bug

--- middleware/session_data_handler/session_data.go
+++ middleware/session_data_handler/session_data.go
@@ -4,15 +4,25 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"strings"
 
 	"github.com/kataras/iris/v12"
 	_const "github.com/lm1996-mojor/go-core-library/const"
 	"github.com/lm1996-mojor/go-core-library/middleware/http_session"
+	"github.com/lm1996-mojor/go-core-library/middleware/security/auth/white_list"
 	"github.com/lm1996-mojor/go-core-library/store"
 	"github.com/lm1996-mojor/go-core-library/utils"
 )
 
 func SessionDataInit(ctx iris.Context) {
+	utils.PrintCallerInfo(ctx)
+	reqPath := ctx.Request().RequestURI
+	ctx.Values().Set("pass_label", "N")
+	if white_list.InList(reqPath, 1) || strings.Contains(reqPath, "platform_management") || strings.Contains(reqPath, "platform_inlet") {
+		ctx.Values().Set("pass_label", "Y")
+		ctx.Next()
+		return
+	}
 	param := make(map[string]interface{})
 	all, _ := io.ReadAll(ctx.Request().Body)
 	json.Unmarshal(all, &param)
@@ -25,6 +35,5 @@ func SessionDataInit(ctx iris.Context) {
 	store.Set(http_session.GetCurrentHttpSessionUniqueKey(ctx)+_const.TokenOriginal, param[_const.TokenOriginal].(string))
 	marshal, _ := json.Marshal(param)
 	ctx.Request().Body = io.NopCloser(bytes.NewReader(marshal))
-	utils.PrintCallerInfo(ctx)
 	ctx.Next()
 }
