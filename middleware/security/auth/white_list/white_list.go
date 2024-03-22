23e9d699a8d4c4c2a79804fef1f261d3d6da482b
lm <1138484503@qq.com>
2024-03-22 14:07:05 +0800

    更新：修改白名单校验逻辑

--- middleware/security/auth/white_list/white_list.go
+++ middleware/security/auth/white_list/white_list.go
@@ -1,28 +1,27 @@
 package white_list
 
 import (
-	"regexp"
+	"strings"
 
 	"github.com/lm1996-mojor/go-core-library/config"
 	"github.com/lm1996-mojor/go-core-library/databases"
 	clog "github.com/lm1996-mojor/go-core-library/log"
 	"github.com/lm1996-mojor/go-core-library/tasker_factory"
-	"github.com/rs/zerolog/log"
 )
 
 type Url struct {
 	ReqUrl    string //接口路径
-	CheckType string //接口检查类型(T:免token A：免鉴权)
+	CheckType int    //接口检查类型(1:免token 2：免鉴权)
 }
 
-var authWhiteListRegex = make([]*regexp.Regexp, 0)
+var tokenWhiteListMap = make(map[string]string)
 
-var tokenWhiteListRegex = make([]*regexp.Regexp, 0)
+var authWhiteListMap = make(map[string]string)
 
 func Init() {
 	clog.Info("初始化路由白名单")
 	defaultWhiteList := make([]Url, 0)
-	defaultWhiteList = append(defaultWhiteList, Url{ReqUrl: "/consul/ser/health", CheckType: "T"})
+	defaultWhiteList = append(defaultWhiteList, Url{ReqUrl: "/consul/ser/health", CheckType: 1})
 	list := InitSystemList()
 	if len(list) > 0 {
 		defaultWhiteList = append(defaultWhiteList, list...)
@@ -32,13 +31,12 @@ func Init() {
 	} else {
 		clog.Info("没有检测要求,无需初始化")
 	}
-	TimedExecution()
+	timedExecution()
 	clog.Info("初始化完成")
 }
 
 func InitSystemList() []Url {
 	defaultWhiteList := make([]Url, 0)
-	defaultWhiteList = append(defaultWhiteList, Url{ReqUrl: "/consul/ser/health", CheckType: "T"})
 	if config.Sysconfig.Detection.Token {
 		list := tokenWhiteListInit()
 		if len(list) > 0 {
@@ -55,31 +53,34 @@ func InitSystemList() []Url {
 }
 
 func tokenWhiteListInit() []Url {
-	defaultWhiteList := make([]Url, 0)
 	clog.Info("获取token白名单....")
+	defaultWhiteList := make([]Url, 0)
 	var tokenWhiteList []string
 	databases.GetDbByName("platform_management").Table("permissions_menu").
-		Where("is_white_list = ?", 1).Where("req_url != '' or req_url is not null").Where("status = ?", 1).Select("req_url").Find(&tokenWhiteList)
+		Where("is_white_list = ?", 1).Where("req_url != '' or req_url is not null").Where("status = ?", 1).
+		Where("req_url like ?", config.Sysconfig.App.GlobalReqPathPrefix+"%").
+		Select("req_url").Find(&tokenWhiteList)
 	for _, url := range tokenWhiteList {
-		defaultWhiteList = append(defaultWhiteList, Url{ReqUrl: url, CheckType: "T"})
+		defaultWhiteList = append(defaultWhiteList, Url{ReqUrl: url, CheckType: 1})
 	}
 	return defaultWhiteList
 }
 
 func authWhiteListInit() []Url {
-	defaultWhiteList := make([]Url, 0)
 	clog.Info("获取权限白名单....")
+	defaultWhiteList := make([]Url, 0)
 	var authWhiteList []string
 	databases.GetDbByName("platform_management").Table("permissions_menu").
 		Where("is_enable_auth = ?", 2).Where("req_url != '' or req_url is not null").Where("status = ?", 1).Where("menu_type = ? or menu_type = ?", 3, 4).
+		Where("req_url like ?", config.Sysconfig.App.GlobalReqPathPrefix+"%").
 		Select("req_url").Find(&authWhiteList)
 	for _, url := range authWhiteList {
-		defaultWhiteList = append(defaultWhiteList, Url{ReqUrl: url, CheckType: "A"})
+		defaultWhiteList = append(defaultWhiteList, Url{ReqUrl: url, CheckType: 2})
 	}
 	return defaultWhiteList
 }
 
-func TimedExecution() {
+func timedExecution() {
 	spec := "@every 11s"
 	err := tasker_factory.AddTask("DelayRefreshList", "发现服务定时任务", spec, DelayRefreshList)
 	if err != nil {
@@ -89,26 +90,12 @@ func TimedExecution() {
 
 func DelayRefreshList() {
 	clog.Info("白名单刷新中...")
-	authWhiteListRegex = make([]*regexp.Regexp, 0)
-	tokenWhiteListRegex = make([]*regexp.Regexp, 0)
 	items := InitSystemList()
-	if len(items) > 0 {
-		var reg *regexp.Regexp
-		var err error
-		for _, item := range items {
-			clog.Infof("新增的白名单：", item)
-			reg, err = regexp.Compile(item.ReqUrl)
-			if err == nil {
-				if item.CheckType == "T" {
-					tokenWhiteListRegex = append(tokenWhiteListRegex, reg)
-				} else if item.CheckType == "A" {
-					authWhiteListRegex = append(authWhiteListRegex, reg)
-				} else {
-					log.Error().Msg("接口检查类型不符合规范，仅支持T（免token）/A（免鉴权）")
-				}
-			} else {
-				log.Error().Msg("invalid regex in white list: " + item.ReqUrl)
-			}
+	for _, item := range items {
+		if item.CheckType == 1 {
+			tokenWhiteListMap[item.ReqUrl] = "TOKEN"
+		} else {
+			authWhiteListMap[item.ReqUrl] = "AUTHENTICATION"
 		}
 	}
 	clog.Info("白名单刷新完成")
@@ -120,42 +107,42 @@ func AppendList(items []Url) {
 		clog.WarnF("新增白名单，传入参数为0,items：", len(items))
 		return
 	}
-	var reg *regexp.Regexp
-	var err error
 	for _, item := range items {
-		clog.Infof("新增的白名单：", item)
-		reg, err = regexp.Compile(item.ReqUrl)
-		if err == nil {
-			if item.CheckType == "T" {
-				tokenWhiteListRegex = append(tokenWhiteListRegex, reg)
-			} else if item.CheckType == "A" {
-				authWhiteListRegex = append(authWhiteListRegex, reg)
-			} else {
-				panic("接口检查类型不符合规范，仅支持T（免token）/A（免鉴权）")
+		if item.CheckType < 1 || item.CheckType > 2 {
+			panic("接口检查类型不符合规范，仅支持T（免token）/A（免鉴权）")
+		}
+		if item.CheckType == 1 {
+			if _, ok := tokenWhiteListMap[item.ReqUrl]; !ok {
+				tokenWhiteListMap[item.ReqUrl] = "TOKEN"
 			}
+			continue
 		} else {
-			log.Error().Msg("invalid regex in white list: " + item.ReqUrl)
+			if _, ok := authWhiteListMap[item.ReqUrl]; !ok {
+				authWhiteListMap[item.ReqUrl] = "AUTHENTICATION"
+			}
+			continue
 		}
 	}
 }
 
 func InList(path string, checkType int) bool {
-	list := make([]*regexp.Regexp, 0)
 	msgStr := ""
 	if checkType == 1 {
 		msgStr = "token"
-		list = tokenWhiteListRegex
+		return match(path, tokenWhiteListMap, msgStr)
 	} else {
 		msgStr = "权限"
-		list = authWhiteListRegex
+		return match(path, authWhiteListMap, msgStr)
 	}
-	//判断请求路径是否在白名单中
-	for _, g := range list {
-		if g.Match([]byte(path)) {
-			clog.Info(path + "：" + msgStr + "白名单匹配结果：成功")
+}
+
+func match(reqPath string, srcReqPathSlice map[string]string, msgStr string) bool {
+	for key, _ := range srcReqPathSlice {
+		if strings.Contains(key, reqPath) {
+			clog.Info(reqPath + "：" + msgStr + "白名单匹配结果：成功")
 			return true
 		}
 	}
-	clog.Info(path + "：" + msgStr + "白名单匹配结果：失败")
+	clog.Info(reqPath + "：" + msgStr + "白名单匹配结果：失败")
 	return false
 }
