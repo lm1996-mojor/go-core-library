5b767a55afaecdd77a84496de4059d5f1eca451f
lm <1138484503@qq.com>
2024-05-18 13:34:49 +0800

    更新：新增服务状态检查机制

--- consul/model_factory.go
+++ consul/model_factory.go
@@ -2,13 +2,9 @@ package consul
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 
 	"github.com/hashicorp/consul/api"
 	localConfig "github.com/lm1996-mojor/go-core-library/config"
-	"github.com/lm1996-mojor/go-core-library/log"
-	"github.com/lm1996-mojor/go-core-library/tasker_factory"
 )
 
 func GetClient() *api.Client {
@@ -31,82 +27,4 @@ type ServiceLibrary struct {
 	Port            int
 	Proto           string
 	Weight          int
-}
-
-func FindSpecifyingServiceList(serviceName string) (serviceList []ServiceLibrary) {
-	if strings.Contains(serviceName, "/") {
-		serviceName = strings.ReplaceAll(serviceName, "/", "")
-	}
-	for _, service := range ServiceLib {
-		if strings.Contains(service.ServiceName, serviceName) {
-			serviceList = append(serviceList, service)
-		}
-	}
-	if len(serviceName) <= 0 {
-		log.Error("没有找到对应服务器")
-		panic("服务器错错误")
-	}
-
-	// 做排序操作：降序（将权重最高的服务放在前面）
-	sort.Slice(serviceList, func(i, j int) bool {
-		return serviceList[i].Weight > serviceList[j].Weight
-	})
-	return serviceList
-}
-
-func ObtainHighestWeightInServiceList(serviceName string) ServiceLibrary {
-	return FindSpecifyingServiceList(serviceName)[0]
-}
-
-func TimedExecution() {
-	spec := "@every 11s"
-	var err error
-	if localConfig.Sysconfig.Consul.EnableObtainService {
-		if localConfig.Sysconfig.Consul.Service.Spec != "" && localConfig.Sysconfig.Consul.Service.Spec != "null" && len(localConfig.Sysconfig.Consul.Service.Spec) > 0 {
-			spec = localConfig.Sysconfig.Consul.Service.Spec
-		}
-		err = tasker_factory.AddTask("ObtainSpecifyingConfigServicesFromTheRegistrationCenter", "发现服务定时任务", spec, ObtainSpecifyingConfigServicesFromTheRegistrationCenter)
-		if err != nil {
-			panic("添加本地服务状态检查定时任务添加失败" + err.Error())
-		}
-	}
-}
-
-func ObtainSpecifyingConfigServicesFromTheRegistrationCenter() {
-	log.Info("获取指定服务列表...")
-
-	serviceMap, err := FindServiceList("")
-	if err != nil {
-		log.Error("Failed to get model list：" + err.Error())
-	}
-	serviceList := make([]ServiceLibrary, 0)
-	for _, service := range serviceMap {
-		serviceList = append(serviceList, ServiceLibrary{
-			ServiceName:     service.Service,
-			ServiceId:       service.ID,
-			ServiceMetadata: service.Meta,
-			Host:            service.Address,
-			Port:            service.Port,
-			Proto:           "http",
-			Weight:          1, // TODO： 后面要动态更改当前的权重情况
-		})
-	}
-
-	if len(localConfig.Sysconfig.Consul.Service.DesignatedServices) <= 0 {
-		ServiceLib = serviceList
-	} else {
-		services := make([]ServiceLibrary, 0)
-		designatedServiceMap := make(map[string]string)
-		for _, designatedService := range localConfig.Sysconfig.Consul.Service.DesignatedServices {
-			designatedServiceMap[designatedService.ServiceName] = designatedService.ServiceName
-		}
-		for _, list := range serviceList {
-			_, ok := designatedServiceMap[list.ServiceName]
-			if ok {
-				services = append(services, list)
-			}
-		}
-		ServiceLib = services
-	}
-	log.Info("获取指定服务列表完成")
 }
--- consul/service.go
+++ consul/service.go
@@ -1,12 +1,16 @@
 package consul
 
 import (
+	"sort"
 	"strings"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/go-uuid"
 	libConfig "github.com/lm1996-mojor/go-core-library/config"
+	_const "github.com/lm1996-mojor/go-core-library/const"
 	"github.com/lm1996-mojor/go-core-library/log"
+	"github.com/lm1996-mojor/go-core-library/store"
+	"github.com/lm1996-mojor/go-core-library/tasker_factory"
 	"github.com/lm1996-mojor/go-core-library/utils/sys_environment"
 	"github.com/spf13/cast"
 )
@@ -90,10 +94,118 @@ func Register() string {
 	return registration.ID
 }
 
+func RefreshRegister() {
+	log.Info("重新注册中...")
+	consulServiceId := Register()
+	store.Set(_const.ConsulEndId, consulServiceId)
+	log.Info("重新注册完成")
+}
+
 func FindServiceList(searchConditionValue string) (map[string]*api.AgentService, error) {
 	return GetClient().Agent().ServicesWithFilter(searchConditionValue)
 }
 
 func ServiceDeregister(serviceId string) error {
 	return GetClient().Agent().ServiceDeregister(serviceId)
+}
+
+func FindSpecifyingServiceList(serviceName string) (serviceList []ServiceLibrary) {
+	if strings.Contains(serviceName, "/") {
+		serviceName = strings.ReplaceAll(serviceName, "/", "")
+	}
+	for _, service := range ServiceLib {
+		if strings.Contains(service.ServiceName, serviceName) {
+			serviceList = append(serviceList, service)
+		}
+	}
+	if len(serviceName) <= 0 {
+		log.Error("没有找到对应服务器")
+		panic("服务器错错误")
+	}
+
+	// 做排序操作：降序（将权重最高的服务放在前面）
+	sort.Slice(serviceList, func(i, j int) bool {
+		return serviceList[i].Weight > serviceList[j].Weight
+	})
+	return serviceList
+}
+
+func ObtainHighestWeightInServiceList(serviceName string) ServiceLibrary {
+	return FindSpecifyingServiceList(serviceName)[0]
+}
+
+func TimedExecution() {
+	spec := "@every 11s"
+	var err error
+	if libConfig.Sysconfig.Consul.EnableObtainService {
+		if libConfig.Sysconfig.Consul.Service.Spec != "" && libConfig.Sysconfig.Consul.Service.Spec != "null" && len(libConfig.Sysconfig.Consul.Service.Spec) > 0 {
+			spec = libConfig.Sysconfig.Consul.Service.Spec
+		}
+		err = tasker_factory.AddTask("ObtainSpecifyingConfigServicesFromTheRegistrationCenter", "发现服务定时任务", spec, ObtainSpecifyingConfigServicesFromTheRegistrationCenter)
+		if err != nil {
+			panic("添加本地服务状态检查定时任务添加失败" + err.Error())
+		}
+	}
+	if libConfig.Sysconfig.Consul.EnableServRegister && libConfig.Sysconfig.Consul.Addr != "" && libConfig.Sysconfig.Consul.Port > 0 {
+		err = tasker_factory.AddTask("ServerStatusCheck", "服务状态定时检查", spec, ServerStatusCheck)
+		if err != nil {
+			panic("添加本地服务状态检查定时任务添加失败" + err.Error())
+		}
+	}
+}
+
+func ServerStatusCheck() {
+	log.Info("服务状态检查中...")
+	value, ok := store.Get(_const.ConsulEndId)
+	if !ok {
+		RefreshRegister()
+	}
+	filter, err := GetClient().Agent().ChecksWithFilter("ServiceID Equal " + value.(string))
+	if err != nil {
+		log.Error("Failed to get check list：" + err.Error())
+		tasker_factory.StopTask("ServerStatusCheck")
+	}
+	if _, flag := filter[value.(string)]; !flag {
+		RefreshRegister()
+	}
+	log.Info("服务状态检查完成")
+}
+
+func ObtainSpecifyingConfigServicesFromTheRegistrationCenter() {
+	log.Info("获取指定服务列表...")
+
+	serviceMap, err := FindServiceList("")
+	if err != nil {
+		log.Error("Failed to get model list：" + err.Error())
+	}
+	serviceList := make([]ServiceLibrary, 0)
+	for _, service := range serviceMap {
+		serviceList = append(serviceList, ServiceLibrary{
+			ServiceName:     service.Service,
+			ServiceId:       service.ID,
+			ServiceMetadata: service.Meta,
+			Host:            service.Address,
+			Port:            service.Port,
+			Proto:           "http",
+			Weight:          1, // TODO： 后面要动态更改当前的权重情况
+		})
+	}
+
+	if len(libConfig.Sysconfig.Consul.Service.DesignatedServices) <= 0 {
+		ServiceLib = serviceList
+	} else {
+		services := make([]ServiceLibrary, 0)
+		designatedServiceMap := make(map[string]string)
+		for _, designatedService := range libConfig.Sysconfig.Consul.Service.DesignatedServices {
+			designatedServiceMap[designatedService.ServiceName] = designatedService.ServiceName
+		}
+		for _, list := range serviceList {
+			_, ok := designatedServiceMap[list.ServiceName]
+			if ok {
+				services = append(services, list)
+			}
+		}
+		ServiceLib = services
+	}
+	log.Info("获取指定服务列表完成")
 }
